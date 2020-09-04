bf0f0faef949460b017d2c9a0fed295752a73483
yu <18113406084@163.com>
2020-09-04 17:57:57 +0800

    subMenu 下拉列表样式，未完善

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,33 @@ import React from 'react';
 import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 import Menu from "./components/Menu/menu";
 import MenuItem from "./components/Menu/menuItem";
+import SubMenu from "./components/Menu/subMnue";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} mode='vertical' onSelect={(index) => {
+        <Menu defaultIndex={0} onSelect={(index) => {
           alert(index)
         }}>
-          <MenuItem index={0}>
+          <MenuItem>
             cool link 0
           </MenuItem>
-          <MenuItem index={1} disabled>
+          <MenuItem disabled>
             cool link 1
           </MenuItem>
-          <MenuItem index={2}>
+          <SubMenu title='dropdown'>
+            <MenuItem>
+              dropdown link 0
+            </MenuItem>
+            <MenuItem>
+              dropdown link 1
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>
             cool link 2
           </MenuItem>
         </Menu>
-
-        <Button className="userClass" onClick={(e) => {
-          e.preventDefault()
-        }}>hello world</Button>
-        <Button btnType={ButtonType.Primary} disabled>disabled Primary</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello world</Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
-        <Button btnType={ButtonType.Link} size={ButtonSize.Small} disabled>Disabled Link</Button>
-        <Button btnType={ButtonType.Link} size={ButtonSize.Small} target='_blank' href='https://www.baidu.com'>Baidu
-          Link</Button>
       </header>
     </div>
   );
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -23,7 +23,8 @@ const Menu: React.FC<MenuProps> = (props) => {
   const {className, mode, style, children, defaultIndex, onSelect} = props
   const [currentActive, setActive] = useState(defaultIndex)
   const classes = classNames('menu', className, {
-    'menu-vertical': mode === 'vertical'
+    'menu-vertical': mode === 'vertical',
+    'menu-horizontal': mode !== 'vertical'
   })
   const handleClick = (index: number) => {
     setActive(index)
@@ -38,7 +39,7 @@ const Menu: React.FC<MenuProps> = (props) => {
     return React.Children.map(children, (child, index) => {
       const childElement = child as React.FunctionComponentElement<MenuItemProps>
       const {displayName} = childElement.type
-      if (displayName === 'MenuItem') {
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
         return React.cloneElement(childElement, {
           index
         })
