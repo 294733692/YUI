e6987bd9e99206d032550d2ef4c014584b52a527
yu <18113406084@163.com>
2020-09-04 10:14:07 +0800

    menu样式编写

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Button, {ButtonSize, ButtonType} from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button className="userClass" onClick={(e) => {e.preventDefault()}}>hello world</Button>
+        <Menu defaultIndex={0} mode='vertical' onSelect={(index) => {
+          alert(index)
+        }}>
+          <MenuItem index={0}>
+            cool link 0
+          </MenuItem>
+          <MenuItem index={1} disabled>
+            cool link 1
+          </MenuItem>
+          <MenuItem index={2}>
+            cool link 2
+          </MenuItem>
+        </Menu>
+
+        <Button className="userClass" onClick={(e) => {
+          e.preventDefault()
+        }}>hello world</Button>
         <Button btnType={ButtonType.Primary} disabled>disabled Primary</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello world</Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
